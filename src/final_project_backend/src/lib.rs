afbdb38cdc37fd4c9f14df8a571818af1a4aa20e
PawanAK <1nc20ci027@ncetmail.com>
2023-12-24 20:47:38 +0530

    Refactor PROPOSAL_MAP initialization in lib.rs

--- src/final_project_backend/src/lib.rs
+++ src/final_project_backend/src/lib.rs
@@ -59,9 +59,12 @@ impl BoundedStorable  for Proposal {
 thread_local! {
     static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> = RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
 
-    static PROPOSAL_MAP: RefCell<StableBTreeMap< u64, Proposal , Memory>> = RefCell::new(StableBTreeMap::init MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(0))));
-
+    static PROPOSAL_MAP: RefCell<StableBTreeMap<u64, Proposal, Memory>> = RefCell::new({
+        let memory_manager = MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(0)));
+        StableBTreeMap::init(memory_manager)
+    });
 }
+
 
 
 #[ic_cdk::query]
